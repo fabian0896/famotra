4087da9da47a6ef3a5ef85ae1d8f04bf4cc15858
Fabian David Dueñas Garcia <fabian0896@outlook.com>
2025-12-15 07:31:55 -0500

    feat: add AlertDialog and Badge components; integrate AlertDialog in DeleteCategoryDialog for category deletion confirmation

--- src/components/category-item.tsx
+++ src/components/category-item.tsx
@@ -1,23 +1,61 @@
 import { useAutoAnimate } from '@formkit/auto-animate/react';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
-import { Edit, Trash2 } from 'lucide-react';
+import { ArrowUpLeft, Trash2 } from 'lucide-react';
 import { useState } from 'react';
 import { toast } from 'sonner';
+import { Sheet, SheetContent, SheetHeader, SheetTrigger } from './ui/sheet';
+import { Badge } from './ui/badge';
+import { Button } from './ui/button';
+import { Separator } from './ui/separator';
 import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuGroup,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from './ui/dropdown-menu';
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from './ui/alert-dialog';
 import { Spinner } from './ui/spinner';
 import type React from 'react';
 import type { Category } from '@/models/categories.models';
 import { cn } from '@/lib/utils';
 import { Categories } from '@/services/categories';
 import { categoriesQueryOptions } from '@/query-options/categories';
+
+function DeleteCategoryDialog({
+  category,
+  children,
+  onConfirm,
+}: {
+  category: Category;
+  children: React.ReactNode;
+  onConfirm: () => void;
+}) {
+  return (
+    <AlertDialog>
+      <AlertDialogTrigger asChild>{children}</AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>Eliminar Categoría {category.name}</AlertDialogTitle>
+          <AlertDialogDescription>
+            ¿Estás seguro que deseas eliminar la categoría{' '}
+            <span className="text-foreground font-semibold">{category.name}</span>? Esta acción
+            también eliminará todas las transacciones asociadas a esta categoría
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancelar</AlertDialogCancel>
+          <AlertDialogAction onClick={onConfirm} variant="destructive">
+            Eliminar categoría
+          </AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
+}
 
 export function CategoryList({ children, className, ...props }: React.ComponentProps<'ul'>) {
   const [parent] = useAutoAnimate();
@@ -29,7 +67,7 @@ export function CategoryList({ children, className, ...props }: React.ComponentP
 }
 
 export function CategoryItem({ category }: { category: Category }) {
-  const [isOpen, setIsOpen] = useState(false);
+  const [sheetOpen, setSheetOpen] = useState(false);
   const queryClient = useQueryClient();
 
   const remove = useMutation({
@@ -43,47 +81,56 @@ export function CategoryItem({ category }: { category: Category }) {
     onSettled: () => {
       const queryKey = categoriesQueryOptions.queryKey;
       queryClient.invalidateQueries({ queryKey });
-      setIsOpen(false);
+      setSheetOpen(false);
     },
   });
+
   return (
     <li>
-      <DropdownMenu open={isOpen} onOpenChange={setIsOpen}>
-        <DropdownMenuTrigger asChild>
-          <button className="flex flex-col gap-1.5">
-            <div className="bg-primary-foreground rounded-full w-16 h-16 text-3xl flex items-center justify-center">
-              <span>{category.icon}</span>
+      <Sheet open={sheetOpen} onOpenChange={setSheetOpen}>
+        <SheetTrigger asChild>
+          <button className="flex flex-col justify-center items-center gap-1.5 group">
+            <div className="bg-primary-foreground rounded-full w-16 h-16 text-2xl flex items-center justify-center">
+              <span className="group-hover:scale-110 group-hover:rotate-12 transition-all">
+                {category.icon}
+              </span>
             </div>
-            <span className="text-muted-foreground text-center font-medium text-sm lowercase first-letter:capitalize">
+            <span className="text-muted-foreground text-center font-medium text-sm lowercase first-letter:capitalize group-hover:text-foreground">
               {category.name}
             </span>
           </button>
-        </DropdownMenuTrigger>
-        <DropdownMenuContent>
-          <DropdownMenuLabel>{category.name}</DropdownMenuLabel>
-          <DropdownMenuSeparator />
-          <DropdownMenuGroup>
-            <DropdownMenuItem>
-              <Edit />
+        </SheetTrigger>
+        <SheetContent>
+          <SheetHeader className="flex flex-col justify-center items-center">
+            <div className="w-22 h-22 rounded-full bg-primary-foreground flex items-center justify-center text-4xl border border-primary">
+              {category.icon}
+            </div>
+            <h2 className="text-4xl font-semibold mt-1">{category.name}</h2>
+            <Badge>
+              <ArrowUpLeft />
+              {category.transaction_type}
+            </Badge>
+          </SheetHeader>
+          <div className="flex gap-4 px-4">
+            <Button className="flex-1" variant="outline">
               Editar
-            </DropdownMenuItem>
-            <DropdownMenuItem
-              onSelect={(event) => {
-                event.preventDefault();
-                remove.mutate({ id: category.id });
-              }}
-              className="text-destructive hover:bg-destructive-foreground"
+            </Button>
+            <DeleteCategoryDialog
+              onConfirm={() => remove.mutate({ id: category.id })}
+              category={category}
             >
-              {remove.isPending ? (
-                <Spinner className="text-destructive" />
-              ) : (
-                <Trash2 className="text-destructive" />
-              )}
-              Eliminar
-            </DropdownMenuItem>
-          </DropdownMenuGroup>
-        </DropdownMenuContent>
-      </DropdownMenu>
+              <Button disabled={remove.isPending} className="flex-1" variant="destructive">
+                {remove.isPending ? <Spinner /> : <Trash2 />}
+                Eliminar
+              </Button>
+            </DeleteCategoryDialog>
+          </div>
+          <Separator className="my-2" />
+          <div className="px-4">
+            <h6 className="font-medium text-foreground text-sm">Transacciones recientes</h6>
+          </div>
+        </SheetContent>
+      </Sheet>
     </li>
   );
 }
